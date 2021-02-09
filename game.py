cdb091dba818b272ed38b28f948bba552f0c8f8b
Johan Thaler <JThaler92@outlook.com>
2021-02-09 14:34:19 +0100

    En hel massa grejer

--- game.py
+++ game.py
@@ -4,17 +4,31 @@ import random
 clock = pygame.time.Clock()
 
 
+def random_snack(rows, item):
+    positions = item.body
+
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+
+    return x, y
+
+
 class Game:
-    TICK_RATE = 60
+    TICK_RATE = 10
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 850
     PLAY_AREA_Y = (50, 800)
     ROWS = 20
     SCREEN_TITLE = 'SnakeSnack'
-    snake_head = pygame.image.load('assets/snake.png')
-    snack_body = pygame.image.load('assets/snack.png')
-    snack_body_scaled = pygame.transform.scale(snack_body, (40, 40))
-    snack = Cube(randomSnack(rows, s), snack_body_scaled)
+    snake_head_file = pygame.image.load('assets/snake.png')
+    snack_body_file = pygame.image.load('assets/snack.png')
+    snake_head = pygame.transform.scale(snake_head_file, (40, 40))
+    snack_body = pygame.transform.scale(snack_body_file, (40, 40))
 
     game_over = False
 
@@ -24,49 +38,60 @@ class Game:
         self.height = self.SCREEN_HEIGHT
         self.game_screen = pygame.display.set_mode((self.width, self.height))
         self.game_screen.fill([0, 0, 0])
+        self.snake = Snake(self.snake_head, (10, 10))
+        self.snack = Cube(random_snack(self.ROWS, self.snake), self.snack_body)
         pygame.display.set_caption(self.title)
 
     def run_game_loop(self):
         game_over = False
-        i = 0  # Initialise rotation: Only for testing
 
         while not game_over:
+            pygame.time.delay(50)
+            clock.tick(self.TICK_RATE)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     game_over = True
-            self.game_screen.fill([0, 0, 0])
-            self.game_screen.blit(pygame.transform.rotate(self.snake_head, i),
-                                  ((self.width - self.snake_head.get_width()) // 2,
-                                   (self.height - self.snake_head.get_height()) // 2))  # Rotate snake: Only for testing
-            pygame.display.update()
-            clock.tick(self.TICK_RATE)
-            i += 1  # Rotate one degree: Only for testing
+            self.redraw_window(self.game_screen)
 
-    def redraw_window(self):
-        pass
-
-    def random_snack(self, rows, item):
-        positions = item.body
-
-        while True:
-            x = random.randrange(rows)
-            y = random.randrange(rows)
-            if len(list(filter(lambda z:z.pos == (x, y), positions))) > 0:
-                continue
-            else:
-                break
-
-        return x, y
+    def redraw_window(self, game_screen):
+        game_screen.fill((0, 0, 0))
+        self.snake.draw(game_screen)
+        self.snack.draw(game_screen, False, True)
+        pygame.display.update()
 
     def collision(self):
         pass
 
 
 class Cube(object):
-    ROWS = 20
+    ROWS = Game.ROWS
     SCREEN_WIDTH = Game.SCREEN_WIDTH
     snake_head = Game.snake_head
-    rotate = pygame.transform.rotate(snake_head, 90)
+    snack_body = Game.snack_body
+
+    def __init__(self, start, dnx=1, dny=0, sprite=snake_head, color=(160, 196, 50)):
+        self.pos = start
+        self.dnx = dnx
+        self.dny = dny
+        self.sprite = sprite
+        self.color = color
+
+    def move(self, dnx, dny):
+        self.dnx = dnx
+        self.dny = dny
+        self.pos = (self.pos[0] + self.dnx, self.pos[1] + self.dny)
+
+    def draw(self, game_screen, head=False, snack=False, rotation=0):
+        dis = self.SCREEN_WIDTH // self.ROWS
+        i = self.pos[0]
+        j = self.pos[1]
+
+        if not head and not snack:
+            pygame.draw.circle(game_screen, self.color, (round(dis * (i + 1 / 2)), round(dis * (j + 1 / 2))), dis // 2)
+        elif snack and not head:
+            pygame.game_screen.blit(self.snack_body, (dis * i, dis * j))
+        elif head and not snack:
+            pygame.game_screen.blit(pygame.transform.rotate(self.snake_head, rotation), (dis * i, dis * j))
 
 
 class Snake(object):
@@ -79,6 +104,7 @@ class Snake(object):
         self.body.append(self.head)
         self.dnx = 0
         self.dny = 0
+        self.rotation = 0
 
     # Move the snake
     def move(self):
@@ -91,21 +117,25 @@ class Snake(object):
                     self.dnx = -1
                     self.dny = 0
                     self.turns[self.head.pos[:]] = [self.dnx, self.dny]
+                    self.rotation = 270
 
                 if keys[pygame.K_RIGHT]:
                     self.dnx = 1
                     self.dny = 0
                     self.turns[self.head.pos[:]] = [self.dnx, self.dny]
+                    self.rotation = 90
 
                 if keys[pygame.K_UP]:
                     self.dnx = 0
                     self.dny = -1
                     self.turns[self.head.pos[:]] = [self.dnx, self.dny]
+                    self.rotation = 180
 
                 if keys[pygame.K_DOWN]:
                     self.dnx = 0
                     self.dny = 1
                     self.turns[self.head.pos[:]] = [self.dnx, self.dny]
+                    self.rotation = 0
 
         # Delete last segment and change positions
         for i, segment in enumerate(self.body):
@@ -141,13 +171,13 @@ class Snake(object):
         tail = self.body[-1]
         dx, dy = tail.dnx, tail.dny
         if dx == 1 and dy == 0:
-            self.body.append(Cube((tail.pos[0]-1, tail.pos[1])))
+            self.body.append(Cube((tail.pos[0] - 1, tail.pos[1])))
         elif dx == -1 and dy == 0:
-            self.body.append(Cube((tail.pos[0]+1, tail.pos[1])))
+            self.body.append(Cube((tail.pos[0] + 1, tail.pos[1])))
         elif dx == 0 and dy == 1:
-            self.body.append(Cube((tail.pos[0], tail.pos[1]-1)))
+            self.body.append(Cube((tail.pos[0], tail.pos[1] - 1)))
         elif dx == 0 and dy == -1:
-            self.body.append(Cube((tail.pos[0], tail.pos[1]+1)))
+            self.body.append(Cube((tail.pos[0], tail.pos[1] + 1)))
 
         self.body[-1].dnx = dx
         self.body[-1].dny = dy
@@ -156,7 +186,6 @@ class Snake(object):
     def draw(self, game_screen):
         for i, segment in enumerate(self.body):
             if i == 0:
-                segment.draw(game_screen, True)
+                segment.draw(game_screen, True, False, self.rotation)
             else:
                 segment.draw(game_screen)
-
