04f1a0f29d31319135b28e454750a1c3f23e896c
Johan Thaler <JThaler92@outlook.com>
2021-02-09 09:55:02 +0100

    More objects in game.py

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ class Game:
     TICK_RATE = 60
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 850
+    PLAY_AREA_Y = (50, 800)
     SCREEN_TITLE = 'SnakeSnack'
     snake_head = pygame.image.load('assets/snake.png')
 
@@ -22,14 +23,36 @@ class Game:
 
     def run_game_loop(self):
         game_over = False
-        i = 0
+        i = 0  # Initialise rotation: Only for testing
 
         while not game_over:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     game_over = True
             self.game_screen.fill([0, 0, 0])
-            self.game_screen.blit(pygame.transform.rotate(self.snake_head, i), ((self.width-self.snake_head.get_width())//2, (self.height-self.snake_head.get_height())//2))
+            self.game_screen.blit(pygame.transform.rotate(self.snake_head, i),
+                                  ((self.width - self.snake_head.get_width()) // 2,
+                                  (self.height - self.snake_head.get_height()) // 2))  # Rotate snake: Only for testing
             pygame.display.update()
             clock.tick(self.TICK_RATE)
-            i += 1
+            i += 1  # Rotate one degree: Only for testing
+
+    def redraw_window(self):
+        pass
+
+    def random_snack(self):
+        pass
+
+    def collision(self):
+        pass
+
+
+class Cube(object):
+    ROWS = 20
+    SCREEN_WIDTH = Game.SCREEN_WIDTH
+
+
+class Snake(object):
+    body = []
+
+
